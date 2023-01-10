54fa33e5304ba6423ab546ba2bf253fc5c4ad949
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-09 19:13:49 -0500

    addthe first component: GifExpertApp

---
+++ 04_gif_expert_app/src/GifExpertApp.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+//FUNCTIONAL COMPONENT
+export const GifExpertApp = () => {
+    return (
+        <Fragment>
+            <h2>GiftExpertApp</h2>
+            <hr />
+        </Fragment>
+    );
+};
