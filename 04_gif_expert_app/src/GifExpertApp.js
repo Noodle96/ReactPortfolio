5710ad668fccee4181a29c2554c2141c9489a595
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-10 13:30:33 -0500

    use useState to render categories and add new element with set function

--- 04_gif_expert_app/src/GifExpertApp.js
+++ 04_gif_expert_app/src/GifExpertApp.js
@@ -1,11 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 //FUNCTIONAL COMPONENT
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['common','rare','legendary']);
+    const handleAdd = () => {
+        // setCategories([...categories, 'newRare']);
+        setCategories(cats => [...cats,'legends']);
+    }
     return (
         <Fragment>
             <h2>GiftExpertApp</h2>
             <hr />
+            <button onClick={(e) => {handleAdd(e)}}>Add Category</button>
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </Fragment>
     );
 };
