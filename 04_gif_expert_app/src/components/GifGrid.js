fcdd624adf688d4c56d917af6c2dd17fc309190c
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-10 23:35:26 -0500

    add GifGridItem component and render the gifs: id, title and url, show it

--- 04_gif_expert_app/src/components/GifGrid.js
+++ 04_gif_expert_app/src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
+    useEffect(() => {
+        getGifs();
+    },[]);
+    // const [contador, setcontador] = useState(0);
+    const [images, setImages] = useState([])
     const getGifs = async() => {
         const url = 'http://api.giphy.com/v1/gifs/search?q=gohan&limit=15&api_key=T7s9dmoxXTBiSi4c6Eh4ZHV6siNEBCpQ';
         const respuesta = await fetch(url);
@@ -14,13 +20,27 @@ export const GifGrid = ({category}) => {
             };
         });
         console.log(gifs);
+        setImages(gifs);
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <div>
+            {/* <h2>{contador}</h2> */}
+            {/* <button onClick={()=>{setcontador(contador+1)}}>testbutton</button> */}
             <h3>{category}</h3>
+            {
+                images.map(img => (
+                    // <li key={img.id}>{img.title}</li>
+                    <GifGridItem 
+                        key={img.id}
+                        // img={img}
+                        {...img}
+                    />
+                ))
+            }
+            
         </div>
     )
 }
