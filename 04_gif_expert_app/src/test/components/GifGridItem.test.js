f45357dea45d65d1866495a5c84750dc352a2d0a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-13 12:05:11 -0500

    strict-test in props of GifGridItems

--- 04_gif_expert_app/src/test/components/GifGridItem.test.js
+++ 04_gif_expert_app/src/test/components/GifGridItem.test.js
@@ -14,4 +14,22 @@ describe('Pruebas en GifGridItem', () => {
     test('Debería de mostrar <GifGridItem /> correctamente ', () => { 
         expect(wrapper).toMatchSnapshot();
      })
+     test('<GifGridItem /> debería de tener un páarafo con el title',() => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+     });
+
+     test('img de <GifGridItem /> deberia de setearse con los elemetos adecuados',() => {
+        const img = wrapper.find('img');
+        // console.log(img.props());
+        expect(img.props().src).toBe(url);
+        expect(img.props().alt).toBe(title);
+     });
+     test('div de <GifGridItem /> deberia de tener la clase  animate_bounce', () => {
+        const div = wrapper.find('div');
+        // console.log(div.props().className);
+        const className = div.props().className;
+        console.log(className);
+        expect(className.includes('animate__bounce')).toBe(true);
+     });
 });
