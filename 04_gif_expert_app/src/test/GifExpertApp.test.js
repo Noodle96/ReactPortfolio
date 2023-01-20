aef29cd1d2e910814958347e4a4c5f3eb4c02749
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-20 10:21:23 -0500

    testing GifExpertApp component - deberia de coincidir en elementos las categorias

--- 04_gif_expert_app/src/test/GifExpertApp.test.js
+++ 04_gif_expert_app/src/test/GifExpertApp.test.js
@@ -6,4 +6,11 @@ describe('Pruebas en el componente GifExpertApp', () => {
         const wrapper = shallow(<GifExpertApp /> );
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('Debería de coincidir en elementos la lista de categorias', () => {
+        const defaultCategories = ['xyz', 'abc'];
+        const wrapper = shallow(<GifExpertApp defaultCategories={defaultCategories} /> );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(defaultCategories.length);
+    });
 });
