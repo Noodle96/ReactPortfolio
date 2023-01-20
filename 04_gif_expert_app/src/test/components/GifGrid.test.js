9395e41e59cfeac35945e896eb66f3fc27ab7a40
Noodle96 <jorgealfredomarino2@gmail.com>
2023-01-20 09:19:59 -0500

    testing GifGrid component - mock useFetchGifs

--- 04_gif_expert_app/src/test/components/GifGrid.test.js
+++ 04_gif_expert_app/src/test/components/GifGrid.test.js
@@ -1,15 +1,46 @@
 import React from "react";
 import {shallow} from 'enzyme';
 import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock('../../hooks/useFetchGifs');
+
 describe('Pruebas en el componente GifGrid', () => {
     const category = 'template category';
+
+    useFetchGifs.mockReturnValue({
+        data: [],
+        loading: true,
+    });
     let wrapper = shallow(<GifGrid category={category}/>);
+
     beforeEach(() => {
+        console.log('hello since beforeEach');
         jest.clearAllMocks();
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
         wrapper = shallow(<GifGrid category={category}/>);
     });
     
     test('El componente <GifGrid /> deberia de mostrarse correctamente', () => { 
         expect(wrapper).toMatchSnapshot();
-    })    
+    });
+
+    test('Debe de mostrar las imagenes retornadas de la funcion useFetchGifs',()=>{
+        console.log('hello since second test');
+        const gifs = [{
+            id: 'template_id',
+            title: 'template_title',
+            url: 'https://templateURL.jpg',
+            type: 'template_type',
+            username: 'template_username ',
+        }];
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false,
+        });
+        wrapper = shallow(<GifGrid category={category}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
 });
