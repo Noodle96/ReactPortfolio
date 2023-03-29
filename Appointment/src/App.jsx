40860634362c6bb3d9a937a2c200e290f3240141
Noodle96 <jorgealfredomarino2@gmail.com>
2023-03-29 17:41:26 -0500

    Add Header'CSS

--- Appointment/src/App.jsx
+++ Appointment/src/App.jsx
@@ -4,12 +4,12 @@ import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   return (
-    <Fragment>
+    <div className="container mx-auto mt-20">
         <Header />
         <Formulario />
         <ListadoPacientes />
 
-    </Fragment>    
+    </div>    
   )
 }
 
--- Appointment/src/components/Header.jsx
+++ Appointment/src/components/Header.jsx
@@ -1,8 +1,11 @@
+
 function Header(){
     return(
-        <header>
-            <h1 >Desde Header</h1>
-        </header>
+        <h1 className="font-black text-2xl md:text-4xl text-center mx-auto md:w-2/3">
+            Seguimiento Pacientes{' '}
+            <span className=" text-indigo-600">Veterinaria</span>
+        </h1>
+
     );
 }
 
