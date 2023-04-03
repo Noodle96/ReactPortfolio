0eb46bd1bda26bad5c343fff0b1a27cf7b32bf31
Noodle96 <jorgealfredomarino2@gmail.com>
2023-04-03 16:22:17 -0500

    Mostrar texto de forma condicional

--- Appointment/src/components/ListadoPacientes.jsx
+++ Appointment/src/components/ListadoPacientes.jsx
@@ -5,20 +5,34 @@ const ListadoPacientes = ({pacientes}) => {
     
   return (
     <div className=' md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll'>
-        <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
-        <p className='text-xl mt-5 mb-10 text-center'>
-            Adminitra tus{' '}
-            <span className=' text-indigo-600 font-bold'>
-                Pacientes y Citas
-            </span>
-        </p>
-        {
-            pacientes.map(paciente => (
-                <Paciente
-                    paciente = {paciente}
-                />
-            ))
-        }
+        {pacientes && pacientes.length ? (
+            <>
+                <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+                <p className='text-xl mt-5 mb-10 text-center'>
+                    Adminitra tus{' '}
+                    <span className=' text-indigo-600 font-bold'>
+                        Pacientes y Citas
+                    </span>
+                </p>
+                {
+                    pacientes.map(paciente => (
+                        <Paciente
+                            paciente = {paciente}
+                        />
+                    ))
+                }
+            </>
+        ) : (
+            <>
+                <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+                <p className='text-xl mt-5 mb-10 text-center'>
+                    Cominenza agregando pacientes{' '}
+                    <span className=' text-indigo-600 font-bold'>
+                        y apareceran en este lugar
+                    </span>
+                </p>
+            </>
+        ) }
     </div>
   )
 }
