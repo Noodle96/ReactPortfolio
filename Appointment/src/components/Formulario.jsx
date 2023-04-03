997545a68a91366b0563892d88a43e59709d568d
Noodle96 <jorgealfredomarino2@gmail.com>
2023-04-02 20:18:32 -0500

    Add Listado de Pacientes

--- Appointment/src/components/Formulario.jsx
+++ Appointment/src/components/Formulario.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Formulario = () => {
   return (
     <div className=' md:w-1/2 lg:w-2/5'>
-         <h2 className=' text-center font-black text-2xl'>
+         <h2 className=' text-center font-black text-3xl'>
             Seguimiento Pacientes
         </h2>
-        <p className=' text-base mt-5 text-center mb-10'>
+        <p className=' text-xl mt-5 text-center mb-10'>
             Añade Pacientes y {''}
             <span className='text-indigo-600 font-black'>Administralos</span>
         </p>
--- Appointment/src/components/ListadoPacientes.jsx
+++ Appointment/src/components/ListadoPacientes.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Paciente from './Paciente'
 
 const ListadoPacientes = () => {
     
   return (
-    <div className=' md:w-1/2 lg:w-3/5'>
-        <h2>Pacientes</h2>
+    <div className=' md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll'>
+        <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+        <p className='text-xl mt-5 mb-10 text-center'>
+            Adminitra tus{' '}
+            <span className=' text-indigo-600 font-bold'>
+                Pacientes y Citas
+            </span>
+        </p>
+        <Paciente />
+        <Paciente />
+        <Paciente />
+
     </div>
   )
 }
