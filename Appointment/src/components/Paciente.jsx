2c3e8734310cb16532c3888ca81af1f9466f485a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-04-04 07:56:10 -0500

    Add buttons(edit and delete)

--- Appointment/src/components/Paciente.jsx
+++ Appointment/src/components/Paciente.jsx
@@ -26,7 +26,21 @@ const Paciente = ({paciente}) => {
                     {sintomas}
                 </span>
             </p>
+            <div className='flex justify-between'>
+                <button
+                    type="button"
+                    className=' bg-indigo-600 hover:bg-indigo-700 py-2 px-10 uppercase text-white font-bold rounded-lg'
+                >
+                    Editar
+                </button>
+                <button
+                    type="button"
+                    className=' bg-red-600 hover:bg-red-700 py-2 px-10 uppercase text-white font-bold rounded-lg'
+                >
+                    Eliminar
+                </button>
             </div>
+        </div>
     )
 }
 
