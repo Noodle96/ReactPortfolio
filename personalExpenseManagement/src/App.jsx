1b14d64a4d56cae2ba95ac24d4f56dc3579103c3
Noodle96 <jorgealfredomarino2@gmail.com>
2023-04-06 10:20:21 -0500

    Add button to new expense and create state modal

--- personalExpenseManagement/src/App.jsx
+++ personalExpenseManagement/src/App.jsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
+import iconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0);
-  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+    const [presupuesto, setPresupuesto] = useState(0);
+    const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+    const [modal, setModal] = useState(false);
 
-  return (
-    <div>
-        <Header
-            presupuesto={presupuesto}
-            setPresupuesto = {setPresupuesto}
-            isValidPresupuesto = {isValidPresupuesto}
-            setIsValidPresupuesto = {setIsValidPresupuesto}
-        />
-    </div>
-  )
+    const handleNuevoGasto = () => {
+        setModal(true);
+    }
+
+    return (
+        <div>
+            <Header
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                isValidPresupuesto={isValidPresupuesto}
+                setIsValidPresupuesto={setIsValidPresupuesto}
+            />
+            {isValidPresupuesto && (
+                <div className="nuevo-gasto">
+                    <img
+                        src={iconoNuevoGasto}
+                        alt="icono nuevo gasto"
+                        onClick={handleNuevoGasto}
+                    />
+                </div>
+            )}
+            {modal && (
+                <p>desde modal</p>
+            )}
+        </div>
+  );
 }
 
-export default App
+export default App;
