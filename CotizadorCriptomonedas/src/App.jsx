b49dc381f71f33072230aaf167c38def436fc62a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-04-22 16:30:16 -0500

    Add Style Components to main Page

--- CotizadorCriptomonedas/src/App.jsx
+++ CotizadorCriptomonedas/src/App.jsx
@@ -1,11 +1,49 @@
 import styled from '@emotion/styled';
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+    max-width: 900px;
+    margin: 0 auto;
+    width: 90%;
+    @media (min-width: 992px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 2rem;
+    }
+`;
+const Imagen = styled.img`
+    max-width: 400px;
+    width: 80%;
+    margin: 100px auto 0 auto;
+    display: block;
+`
+
 const Heading = styled.h1`
-    font-family: 'lato', sans-serif;
+    font-family: 'Lato', sans-serif;
     color: #FFF;
+    text-align: center;
+    font-weight: 700;
+    margin-top: 80px;
+    margin-bottom: 50px;
+    font-size: 34px;
+    &::after {
+        content: '';
+        width: 200px;
+        height: 3px;
+        background-color: #66A2FE;
+        display: block;
+        margin: 10px auto 0 auto;
+    }
 `;
 function App() {
     return (
-        <Heading>Desde App</Heading>
+        <Contenedor>
+            <Imagen 
+                src={ImagenCripto}
+                alt="Imagen cripto"
+            />
+            <Heading>Cotiza Criptomonedas al instante</Heading>
+        </Contenedor>
     )
 }
 
