27bc79b236bf8f05e6e3da53606eedef6056fa50
Noodle96 <jorgealfredomarino2@gmail.com>
2023-05-04 15:47:18 -0500

    Formatenado el formulario

--- CotizadorCriptomonedas/src/components/Resultado.jsx
+++ CotizadorCriptomonedas/src/components/Resultado.jsx
@@ -1,15 +1,52 @@
 import styled from "@emotion/styled"
+
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`;
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`;
+
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+        color: #0380f5;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 24px;
+    span {
+        font-weight: 700;
+        color: #18f503;
+    }
+`;
+
 const Resultado = ({resultado}) => {
     // console.log(resultado);
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL,LASTUPDATE} = resultado;
     return (
-        <div>
-            <p>El precio es de: <span>{PRICE}</span></p>
-            <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-            <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-            <p>Variación últimas 24 hrs: <span>{CHANGEPCT24HOUR}</span></p>
-            <p>Última actualización: <span>{LASTUPDATE}</span></p>
-        </div>
+        <Contenedor>
+            <Imagen
+                src={`https://cryptocompare.com/${IMAGEURL}`}
+                alt="imagen cripto"
+            />
+            <div>
+                <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+                <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+                <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+                <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+                <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+            </div>
+        </Contenedor>
     )
 }
 
