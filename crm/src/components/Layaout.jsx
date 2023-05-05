7626e4051301829bf5f9fd5dab481eb2d33f76c4
Noodle96 <jorgealfredomarino2@gmail.com>
2023-05-05 16:45:57 -0500

    Navegar entre componentes con Link

--- crm/src/components/Layaout.jsx
+++ crm/src/components/Layaout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom';
+import {Outlet, Link} from 'react-router-dom';
 const Layaout = () => {
 return (
     <div className='md:flex md:min-h-screen'>
         <aside className=' md:w-1/4 bg-blue-600 px-5 py-10'>
             <h2 className=' text-2xl font-black text-center text-white'>CRM - Clientes</h2>
+            <Link className=' text-lg block mt-2 hover:text-blue-300 text-white' to="/">Clientes</Link>
+            <Link className=' text-lg block mt-2 hover:text-blue-300 text-white' to="/clientes/nuevo">Nuevo Cliente</Link>
         </aside>
         <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
             <Outlet />
