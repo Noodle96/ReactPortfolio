eb94796db68882ed5f23e15093d643139fd9648f
Noodle96 <jorgealfredomarino2@gmail.com>
2023-05-05 16:34:44 -0500

    Creando el layout principal

--- crm/src/components/Layaout.jsx
+++ crm/src/components/Layaout.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {Outlet} from 'react-router-dom';
 const Layaout = () => {
 return (
-    <div>
-        <h1 className=' text-6xl font-bold'>CRM - React</h1>
-        <Outlet />
+    <div className='md:flex md:min-h-screen'>
+        <aside className=' md:w-1/4 bg-blue-600 px-5 py-10'>
+            <h2 className=' text-2xl font-black text-center text-white'>CRM - Clientes</h2>
+        </aside>
+        <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+            <Outlet />
+        </main>
     </div>
 )
 }
