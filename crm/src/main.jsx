301d18a81208f76777ac40eba6490ba9abdd93cc
Noodle96 <jorgealfredomarino2@gmail.com>
2023-05-05 15:42:03 -0500

    Create router and add new routes

--- crm/src/main.jsx
+++ crm/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <h1>Desde Inicio</h1>
+    },
+    {
+        path:'/nosotros',
+        element: <h1>Desde Nosotros</h1>
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {/* //instead of <App /> => <RouterProvider />*/}
+        <RouterProvider
+            router={router}
+        />
+    </React.StrictMode>,
 )
