b671a1d02f5ab7ff24fcd298b1ae9602b7d6d95b
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-22 14:08:40 -0500

    Añadir hojas de estilos externas

--- guitarla-remix/app/root.jsx
+++ guitarla-remix/app/root.jsx
@@ -18,8 +18,25 @@ export  function links(){
 	return[
 		{
 			rel: 'stylesheet',
+			href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css',
+		},
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.googleapis.com',
+		},
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.gstatic.com',
+			crossOrigin: "true",
+		},
+		{
+			rel: 'stylesheet',
+			href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap',
+		},
+		{
+			rel: 'stylesheet',
 			href: styles,
-		}
+		},
 	];
 }
 export default function App(){
