216cfb27ac5c0b2367f7e9a21a590977469a0dcc
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-22 14:00:06 -0500

    Añadiendo hojas de css

--- guitarla-remix/app/root.jsx
+++ guitarla-remix/app/root.jsx
@@ -1,6 +1,8 @@
 import {
-	Meta
+	Meta,
+	Links,
 } from '@remix-run/react'
+import styles from './styles/index.css';
 
 export function meta() {
     return (
@@ -12,6 +14,14 @@ export function meta() {
     )
 }
 
+export  function links(){
+	return[
+		{
+			rel: 'stylesheet',
+			href: styles,
+		}
+	];
+}
 export default function App(){
 	return (
 		<Document>
@@ -25,6 +35,7 @@ function Document({children}) {
         <html lang="es">
             <head>
                  <Meta />
+				 <Links />
             </head>
             <body>
                 {children}
