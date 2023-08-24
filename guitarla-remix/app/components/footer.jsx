0a0029d5b3d8521db184e5816c4cbbb879afaeda
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-24 13:26:12 -0500

    HTML to nosotros page

--- guitarla-remix/app/components/footer.jsx
+++ guitarla-remix/app/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 	<footer className='footer'>
 		<div className="contenedor contenido">
 			<Navegacion />
-			<p className="copyright">Todos los derechso r3servados {new Date().getFullYear()}</p>
+			<p className="copyright">Todos los derechos reservados {new Date().getFullYear()}</p>
 		</div>
 	</footer>
   )
--- guitarla-remix/app/routes/nosotros.jsx
+++ guitarla-remix/app/routes/nosotros.jsx
@@ -1,8 +1,30 @@
-
+import imagen_nosotros from '~/../public/img/nosotros.jpg';
 function Nosotros() {
-  return (
-	<div>Nosotros</div>
-  )
+    return(
+        <main className='contenedor nosotros'>
+            <h2 className="heading">Nosotros</h2>
+            <div className="contenido">
+                <img src={imagen_nosotros} alt="imagen sobre nosotros" />
+                <div>
+                    <p>
+                        Nam nec neque eget nunc ullamcorper vehicula. Pellentesque habitant morbi
+                        tristique senectus et netus et malesuada fames ac turpis egestas. Duis
+                        hendrerit sit amet lacus ut tristique. Integer ipsum metus, efficitur
+                        eu magna ut, commodo efficitur arcu. Nunc sit amet lorem sit amet ipsum
+                        tempor pretium. Donec placerat sem in enim elementum venenatis. Cras
+                    </p>
+
+                    <p>
+                        Nam nec neque eget nunc ullamcorper vehicula. Pellentesque habitant morbi
+                        tristique senectus et netus et malesuada fames ac turpis egestas. Duis
+                        hendrerit sit amet lacus ut tristique. Integer ipsum metus, efficitur
+                        eu magna ut, commodo efficitur arcu. Nunc sit amet lorem sit amet ipsum
+                        tempor pretium. Donec placerat sem in enim elementum venenatis. Cras
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default Nosotros
