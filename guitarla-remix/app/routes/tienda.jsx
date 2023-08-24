ee6b27fc66aa5f656babd0de4395bcafab452291
Noodle96 <jorgealfredomarino2@gmail.com>
2023-08-24 17:32:36 -0500

    consumir el backend de strapi con loader

--- guitarla-remix/app/routes/tienda.jsx
+++ guitarla-remix/app/routes/tienda.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+
+export async function loader(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const resultado = await respuesta.json();
+  console.log(resultado);
+  console.log('desde loader tienda');
+  console.log(process.env.API_URL);
+  return {};
+}
 
 function Tienda() {
   return (
