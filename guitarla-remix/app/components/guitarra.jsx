a44317c9d487ae659a7cb9095669b5f4210da6cc
Noodle96 <jorgealfredomarino2@gmail.com>
2023-09-01 13:58:02 -0500

    create component guitarra and add to tienda

---
+++ guitarla-remix/app/components/guitarra.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Guitarra() {
+	return (
+		<div>
+			guitarra
+		</div>
+	)
+}
--- guitarla-remix/app/routes/tienda.jsx
+++ guitarla-remix/app/routes/tienda.jsx
@@ -1,13 +1,35 @@
+import {useLoaderData} from '@remix-run/react'; 
 import {getGuitarras} from '~/models/guitarras.server';
+import Guitarra from '~/components/guitarra';
+
+
 export async function loader(){
   const guitarras = await getGuitarras();
   console.log(guitarras);
-  return guitarras;
+  return guitarras.data;
 }
 
-function Tienda() {
+function Tienda(){
+  const guitarras = useLoaderData();
+  // console.log(guitarras);
   return (
-	<div>Tienda</div>
+    <main className='contenedor'>
+      <h2 className="heading">Nuestra Coleccion</h2>
+      {
+        guitarras.length && (
+          <div className="guitarras-grid">
+            {
+              guitarras.map(guitarra => (
+                <Guitarra  
+                  
+                />
+                
+              ))
+            }
+          </div>
+        )
+      }
+    </main>
   )
 }
 
