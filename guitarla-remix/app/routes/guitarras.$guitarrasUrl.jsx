7b5e5ba6e06ed64d6f300577ebee6e23ead8b323
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-25 12:41:50 -0500

    Mostrando informacion de las guitarras

--- guitarla-remix/app/routes/guitarras.$guitarrasUrl.jsx
+++ guitarla-remix/app/routes/guitarras.$guitarrasUrl.jsx
@@ -1,5 +1,16 @@
 import { useLoaderData } from '@remix-run/react';
 import {getGuitarra} from '~/models/guitarras.server';
+import styles from '~/styles/guitarras.css';
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles,
+    }
+  ];
+}
+
 export async function loader({params}){
   //console.log(params); // { guitarrasUrl: 'urlName' }
   const {guitarrasUrl} = params;
@@ -14,9 +25,18 @@ export async function loader({params}){
 function GuitarrasUrl() {
   const guitarra = useLoaderData();
   console.log("In Console");
+  //console.log(guitarra);
   console.log(guitarra.data[0].attributes.nombre);
+  const {nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
   return (
-	<div>GuitarrasUrl</div>
+    <main className='contenedor guitarra'>
+      <img src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} className="imagen" />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
   )
 }
 
