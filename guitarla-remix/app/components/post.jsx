e1f67bd62754b9ab92b8230054eb87e78a71f655
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-26 11:57:20 -0500

    Creando una funcion para formatear la fecha

--- guitarla-remix/app/components/post.jsx
+++ guitarla-remix/app/components/post.jsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react";
-
+import {formatearFecha} from '~/utils/helpers';
 export default function Post({post}) {
     //console.log(post);
     const {titulo, contenido, imagen ,url, publishedAt} = post;
@@ -8,7 +8,7 @@ export default function Post({post}) {
             <img src={imagen.data.attributes.formats.small.url} alt={`Posts ${titulo}`} className="imagen" />
             <div className="contenido">
                 <h3>{titulo}</h3>
-                <p className="fecha">{publishedAt}</p>
+                <p className="fecha">{formatearFecha(publishedAt)}</p>
                 <p className="resumen">{contenido}</p>
                 {/* <p className="url">{url}</p> */}
                 <Link className="enlace" to={`/posts/${url}`}>Leer Post</Link>
