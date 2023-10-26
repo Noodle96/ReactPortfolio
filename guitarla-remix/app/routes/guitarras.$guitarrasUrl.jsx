68f7e84b8632578f80e906b6e9192d85b59a34b8
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-26 11:19:01 -0500

    Refactoring guitarras.DOLARguitarrasUrl.jsx

--- guitarla-remix/app/routes/guitarras.$guitarrasUrl.jsx
+++ guitarla-remix/app/routes/guitarras.$guitarrasUrl.jsx
@@ -3,20 +3,20 @@ import {getGuitarra} from '~/models/guitarras.server';
 import styles from '~/styles/guitarras.css';
 
 export async function loader({params}){
-  //console.log(params); // { guitarrasUrl: 'urlName' }
-  const {guitarrasUrl} = params;
-  //console.log(guitarrasUrl); // urlName
-  const guitarra = await getGuitarra(guitarrasUrl);
-  console.log("Terminal: Desde await getGuitarra() in loader of guitarras$file");
-  //console.log(guitarra.data[0].attributes.nombre);
-  if(guitarra.data.length === 0){
-    console.log("file.$name data vacio");
-    throw new Response('',{
-      status: 404,
-      statusText: 'Guitarra no encontrada',
-    });
-  }
-  return guitarra;
+    //console.log(params); // { guitarrasUrl: 'urlName' }
+    const {guitarrasUrl} = params;
+    //console.log(guitarrasUrl); // urlName
+    const guitarra = await getGuitarra(guitarrasUrl);
+    console.log("Terminal: Desde await getGuitarra() in loader of guitarras$file");
+    //console.log(guitarra.data[0].attributes.nombre);
+    if(guitarra.data.length === 0){
+        console.log("file.$name data vacio");
+        throw new Response('',{
+            status: 404,
+            statusText: 'Guitarra no encontrada',
+        });
+    }
+    return guitarra;
 }
 
 export function meta({data}){
@@ -49,21 +49,21 @@ export function links(){
 
 
 function GuitarrasUrl() {
-  const guitarra = useLoaderData();
-  console.log("In Console");
-  //console.log(guitarra);
-  console.log(guitarra.data[0].attributes.nombre);
-  const {nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
-  return (
-    <main className='contenedor guitarra'>
-      <img src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} className="imagen" />
-      <div className="contenido">
-        <h3>{nombre}</h3>
-        <p className="texto">{descripcion}</p>
-        <p className="precio">${precio}</p>
-      </div>
-    </main>
-  )
+    const guitarra = useLoaderData();
+    console.log("In Console");
+    //console.log(guitarra);
+    console.log(guitarra.data[0].attributes.nombre);
+    const {nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
+    return (
+        <main className='contenedor guitarra'>
+            <img src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} className="imagen" />
+            <div className="contenido">
+                <h3>{nombre}</h3>
+                <p className="texto">{descripcion}</p>
+                <p className="precio">${precio}</p>
+            </div>
+        </main>
+    )
 }
 
 export default GuitarrasUrl
