f8d6d5022e974aa221266bffcb57c2e0a22824ff
Noodle96 <jorgealfredomarino2@gmail.com>
2023-10-31 16:35:07 -0500

    Agregando contenido y CSS a la seccion de carrito

--- guitarla-remix/app/routes/carrito.jsx
+++ guitarla-remix/app/routes/carrito.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import styles from '~/styles/carrito.css';
 
+export function meta() {
+    return (
+        [
+            {title: 'GuitarLA - carrito de compras'},
+            {description: 'seccion carrito de compras'}
+		]
+    )
+}
+
+export function links(){
+	return [
+		{
+			rel: 'stylesheet',
+			href: styles,
+		}
+	];
+}
 export default function Carrito() {
   return (
-	<div>
-		carrito de commpras
-	</div>
+	<main className="contenedor">
+		<h1 className="heading">Carrito de compras</h1>
+		<div className="contenido">
+			<div className="carrito">
+				<h2>Articulos</h2>
+			</div>
+			<aside className="resumen">
+				<h3>Resumen del pedido</h3>
+				<p>Total a pagar: $</p>
+			</aside>
+		</div>
+		
+	</main>
   )
 }
